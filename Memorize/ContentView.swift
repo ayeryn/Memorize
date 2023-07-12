e5ffed2c5a1b245ba67ae1d499908553fcbb6c89
ayeryn <erynli@utexas.edu>
2023-07-12 18:15:20 -0500

    Init lecture branch

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,72 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
-    let carEmojis = ["✈️", "🚗", "🚄", "🚀", "🚛", "🚁", "🚌", "🛴", "🛵", "🛸", "🚲", "🚎", "🚕", "🏎️", "🚜", "🏍️","🚙"]
-    let sportEmojis = ["🏋️‍♀️","🏂","⛹️‍♀️","🏌️‍♀️","🏄‍♂️","🤼‍♂️","🚵‍♀️","🧗‍♀️","🚣‍♀️","🏊‍♀️"]
-    let weatherEmojis = ["☀️","🌤️","☁️","🌧️","❄️","🌪️","⛄️","🌈","🌩️"]
-    @State var emojis = ["✈️", "🚗", "🚄", "🚀", "🚛", "🚁", "🚌", "🛴", "🛵", "🛸", "🚲", "🚎", "🚕", "🏎️", "🚜", "🏍️","🚙"]
+    var emojis = ["✈️", "🚗", "🚄", "🚀", "🚛", "🚁", "🚌", "🛴", "🛵", "🛸", "🚲", "🚎", "🚕", "🏎️", "🚜", "🏍️","🚙"]
+    @State var emojiCount = 16
     var body: some View {
         VStack{
-            Text("Memorize!")
-                .font(.largeTitle)
-            Spacer()
-            ScrollView{
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]){
-                    ForEach(emojis, id: \.self){ emoji in
-                        CardView(emoji: emoji)
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]){
+                    /* CARDS */
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
                     }
-                    .aspectRatio(2/3, contentMode: .fit)
                 }
             }
-            .padding(.horizontal)
+            .foregroundColor(.pink)
             Spacer()
-            HStack{
-                Button {
-                    emojis = carEmojis.shuffled()
-                } label: {
-                    VStack{
-                        Image(systemName: "car")
-                        Text("Vehicles")
-                            .font(.caption)
-                    }
-                }.buttonStyle(.bordered)
-                Button {
-                    emojis = sportEmojis.shuffled()
-                } label: {
-                    VStack{
-                        Image(systemName: "figure.run")
-                        Text("Sports")
-                            .font(.caption)
-                    }
-                }.buttonStyle(.bordered)
-                Button{
-                    emojis = weatherEmojis.shuffled()
-                } label: {
-                    VStack{
-                        Image(systemName: "sun.max")
-                        Text("Weathers")
-                            .font(.caption)
-                    }
-                }.buttonStyle(.bordered)
+            HStack {
+                /* BUTTONS */
+                remove
+                Spacer()
+                // Grabs as much space it can
+                add
             }
+            .font(.largeTitle)
+            .padding(.horizontal)
+        }
+        .padding(.horizontal)
+    }
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+    }
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
         }
     }
 }
 
-
-struct CardView : View{
-    var card = RoundedRectangle(cornerRadius: 20)
-    var emoji: String
-    @State var isFaceUp = true
-    let color: Color = .pink
+// Modularization my friends!!
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    // changes var to a pointer that points to a bool
+    
     var body: some View {
         ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
             if isFaceUp {
-                card.fill(.white)
-                card.strokeBorder(color, lineWidth: 3)
-                Text(emoji).font(.largeTitle)
-            } else {
-                card.fill(color)
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 5)
+                Text(content).font(.largeTitle)
+            }
+            else {
+                shape.fill()
             }
         }
         .onTapGesture {
